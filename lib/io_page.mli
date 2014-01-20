mirage/io-page

commit e25686d9ea4661b0615256360ded9fd42212e2fc

Do not depend directly on the mirage-types signature to help break a circular dependency.  The `portable` test still exists to check against when changing anything.

lib/io_page.mli
@@ -17,4 +17,47 @@
 
 (** Memory allocation. *)
 
-include V1.IO_PAGE with type buf = Cstruct.t
+(** Memory allocation interface. *)
+
+type buf
+(** Type of a C buffer (usually Cstruct) *)
+
+type t = (char, Bigarray.int8_unsigned_elt, Bigarray.c_layout) Bigarray.Array1.t
+(** Type of memory blocks. *)
+
+val get : int -> t
+(** [get n] allocates and returns a memory block of [n] pages. If
+    there is not enough memory, the unikernel will terminate. *)
+
+val get_order : int -> t
+(** [get_order i] is [get (1 lsl i)]. *)
+
+val pages : int -> t list
+(** [pages n] allocates a memory block of [n] pages and return the the
+    list of pages allocated. *)
+
+val pages_order : int -> t list
+(** [pages_order i] is [pages (1 lsl i)]. *)
+
+val length : t -> int
+(** [length t] is the size of [t], in bytes. *)
+
+val to_cstruct : t -> buf
+val to_string : t -> string
+
+val to_pages : t -> t list
+(** [to_pages t] is a list of [size] memory blocks of one page each,
+    where [size] is the size of [t] in pages. *)
+
+val string_blit : string -> int -> t -> int -> int -> unit
+(** [string_blit src srcoff dst dstoff len] copies [len] bytes from
+    string [src], starting at byte number [srcoff], to memory block
+    [dst], starting at byte number dstoff. *)
+
+val blit : t -> t -> unit
+(** [blit t1 t2] is the same as {!Bigarray.Array1.blit}. *)
+
+val round_to_page_size : int -> int
+(** [round_to_page_size n] returns the number of bytes that will be
+    allocated for storing [n] bytes in memory *)
+

_oasis
@@ -1,6 +1,6 @@
 OASISFormat: 0.3
 Name:        io-page
-Version:     1.0.0
+Version:     1.0.1
 Synopsis:    Page-aligned memory allocator
 Authors:     Anil Madhavapeddy, David Scott, Thomas Gazagnaire
 License:     ISC
@@ -16,7 +16,7 @@ Library io_page
   Path:               lib
   Findlibname:        io-page
   Modules:            Io_page
-  BuildDepends:       cstruct, mirage-types
+  BuildDepends:       cstruct
 
 Library io_page_unix
   Build$:             flag(unix)
@@ -29,13 +29,13 @@ Library io_page_unix
   CSources:           stub_alloc_pages.c
 
 Executable portable
-  Build$:             flag(tests)
+  Build:             false
   CompiledObject:     best
   Path:               lib_test
   MainIs:             portable.ml
   Custom:             true
   Install:            false
-  BuildDepends:       io-page.unix, io-page, mirage-types, oUnit
+  BuildDepends:       io-page.unix, io-page, mirage.types, oUnit
   CSources:           stub_get_addr.c
 
 Test portable

_tags
@@ -1,5 +1,5 @@
 # OASIS_START
-# DO NOT EDIT (digest: 32363e3d2e49bfec8e0263a29f315c01)
+# DO NOT EDIT (digest: d4f3a786fdfc8485e7eec4bf1555f09d)
 # Ignore VCS directories, you can use the same kind of rule outside
 # OASIS_START/STOP if you want to exclude directories that contains
 # useless stuff for the build process
@@ -15,7 +15,6 @@
 "_darcs": not_hygienic
 # Library io_page
 "lib/io_page.cmxs": use_io_page
-<lib/*.ml{,i}>: pkg_mirage-types
 <lib/*.ml{,i}>: pkg_cstruct
 # Library io_page_unix
 "lib/io_page_unix.cmxs": use_io_page_unix
@@ -26,19 +25,19 @@
 <lib_test/portable.{native,byte}>: use_libportable_stubs
 <lib_test/portable.{native,byte}>: use_io_page
 <lib_test/portable.{native,byte}>: use_io_page_unix
-<lib_test/portable.{native,byte}>: pkg_mirage-types
+<lib_test/portable.{native,byte}>: pkg_mirage.types
 <lib_test/portable.{native,byte}>: pkg_oUnit
 <lib_test/portable.{native,byte}>: pkg_bigarray
 <lib_test/portable.{native,byte}>: pkg_cstruct
 <lib_test/*.ml{,i}>: use_io_page
 <lib_test/*.ml{,i}>: use_io_page_unix
-<lib_test/*.ml{,i}>: pkg_mirage-types
+<lib_test/*.ml{,i}>: pkg_mirage.types
 <lib_test/*.ml{,i}>: pkg_oUnit
 <lib_test/*.ml{,i}>: pkg_bigarray
 <lib_test/*.ml{,i}>: pkg_cstruct
 "lib_test/stub_get_addr.c": use_io_page
 "lib_test/stub_get_addr.c": use_io_page_unix
-"lib_test/stub_get_addr.c": pkg_mirage-types
+"lib_test/stub_get_addr.c": pkg_mirage.types
 "lib_test/stub_get_addr.c": pkg_oUnit
 "lib_test/stub_get_addr.c": pkg_bigarray
 "lib_test/stub_get_addr.c": pkg_cstruct

lib/META
@@ -1,15 +1,15 @@
 # OASIS_START
-# DO NOT EDIT (digest: 9462611e2382968328a24258767ae75e)
-version = "1.0.0"
+# DO NOT EDIT (digest: bb9279ba676fcc7000e10742d69f895b)
+version = "1.0.1"
 description = "Page-aligned memory allocator"
-requires = "cstruct mirage-types"
+requires = "cstruct"
 archive(byte) = "io_page.cma"
 archive(byte, plugin) = "io_page.cma"
 archive(native) = "io_page.cmxa"
 archive(native, plugin) = "io_page.cmxs"
 exists_if = "io_page.cma"
 package "unix" (
- version = "1.0.0"
+ version = "1.0.1"
  description = "Page-aligned memory allocator"
  requires = "bigarray"
  archive(byte) = "io_page_unix.cma"

setup.ml
@@ -1,7 +1,7 @@
 (* setup.ml generated for the first time by OASIS v0.3.0 *)
 
 (* OASIS_START *)
-(* DO NOT EDIT (digest: e5d2a8c103bf35335f0cdfb79ef6c41e) *)
+(* DO NOT EDIT (digest: 43d913a334c1ac203b27e3c36d874c8b) *)
 (*
    Regenerated by OASIS v0.4.1
    Visit http://oasis.forge.ocamlcore.org for more information and
@@ -6730,7 +6730,7 @@ let setup_t =
           alpha_features = [];
           beta_features = [];
           name = "io-page";
-          version = "1.0.0";
+          version = "1.0.1";
           license =
             OASISLicense.DEP5License
               (OASISLicense.DEP5Unit
@@ -6804,11 +6804,7 @@ let setup_t =
                       bs_install = [(OASISExpr.EBool true, true)];
                       bs_path = "lib";
                       bs_compiled_object = Best;
-                      bs_build_depends =
-                        [
-                           FindlibPackage ("cstruct", None);
-                           FindlibPackage ("mirage-types", None)
-                        ];
+                      bs_build_depends = [FindlibPackage ("cstruct", None)];
                       bs_build_tools = [ExternalTool "ocamlbuild"];
                       bs_c_sources = [];
                       bs_data_files = [];
@@ -6868,11 +6864,7 @@ let setup_t =
                      cs_plugin_data = []
                   },
                    {
-                      bs_build =
-                        [
-                           (OASISExpr.EBool true, false);
-                           (OASISExpr.EFlag "tests", true)
-                        ];
+                      bs_build = [(OASISExpr.EBool true, false)];
                       bs_install = [(OASISExpr.EBool true, false)];
                       bs_path = "lib_test";
                       bs_compiled_object = Best;
@@ -6880,7 +6872,7 @@ let setup_t =
                         [
                            InternalLibrary "io_page_unix";
                            InternalLibrary "io_page";
-                           FindlibPackage ("mirage-types", None);
+                           FindlibPackage ("mirage.types", None);
                            FindlibPackage ("oUnit", None)
                         ];
                       bs_build_tools = [ExternalTool "ocamlbuild"];
@@ -6925,7 +6917,7 @@ let setup_t =
        };
      oasis_fn = Some "_oasis";
      oasis_version = "0.4.1";
-     oasis_digest = Some "O\249,!\019G\214\132\227^\186:\r\210\2006";
+     oasis_digest = Some "iX¶\003\153ÖÐûN\025æð#¥\142'";
      oasis_exec = None;
      oasis_setup_args = [];
      setup_update = false
@@ -6933,6 +6925,6 @@ let setup_t =
 
 let setup () = BaseSetup.setup setup_t;;
 
-# 6937 "setup.ml"
+# 6929 "setup.ml"
 (* OASIS_STOP *)
 let () = setup ();;

commit 87ca4cb36e62a6ae11168aac80be227feec33211

Make the Io_page to cstruct a little less abstract
(This was a regression in e25686d9ea4661b)

Spotted by @djs55

lib/io_page.mli
@@ -19,8 +19,8 @@
 
 (** Memory allocation interface. *)
 
-type buf
-(** Type of a C buffer (usually Cstruct) *)
+type buf = Cstruct.t
+(** Type of a C buffer (in this case, a Cstruct) *)
 
 type t = (char, Bigarray.int8_unsigned_elt, Bigarray.c_layout) Bigarray.Array1.t
 (** Type of memory blocks. *)