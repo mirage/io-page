de8c5ad5682e57c822bc5b4c93998bd4657acb0d
Anil Madhavapeddy <anil@recoil.org>
2014-11-21 16:13:58 +0000

    Add OPAM 1.2 files

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,27 +1,11 @@
-# OPAM packages needed to build tests.
-OPAM_PACKAGES="cstruct mirage-types"
+PACKAGE=io-page
+wget https://raw.githubusercontent.com/samoht/ocaml-travisci-skeleton/master/.travis-opam.sh
+sh .travis-opam.sh
 
-case "$OCAML_VERSION,$OPAM_VERSION" in
-3.12.1,1.1.0) ppa=avsm/ocaml312+opam11 ;;
-4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
-4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
-4.02.1,1.1.0) ppa=avsm/ocaml42+opam11 ;;
-3.12.1,1.2.0) ppa=avsm/ocaml312+opam12 ;;
-4.00.1,1.2.0) ppa=avsm/ocaml40+opam12 ;;
-4.01.0,1.2.0) ppa=avsm/ocaml41+opam12 ;;
-4.02.1,1.2.0) ppa=avsm/ocaml42+opam12 ;;
-*) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
-esac
-
-echo "yes" | sudo add-apt-repository ppa:$ppa
-sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 export OPAMYES=1
-export OPAMVERBOSE=1
-
-opam init
-opam install ocamlfind cstruct ounit mirage-types
 eval `opam config env`
-opam install ${OPAM_PACKAGES}
+prefix=`opam config var prefix`
+
+./configure --prefix=$prefix --enable-tests
 make
-make install
+make test
