1b089cf68222b987c2e969b7e784f3c8dd666ee1
Hannes Mehnert <hannes@mehnert.org>
2021-03-26 12:14:23 +0100

    drop "caml_" prefix from C code

--- lib/stub_alloc_pages.c
+++ lib/stub_alloc_pages.c
@@ -47,7 +47,7 @@
    call free() whenever all sub-bigarrays are unreachable.
  */
 CAMLprim value
-caml_mirage_iopage_alloc_pages(value did_gc, value n_pages)
+mirage_iopage_alloc_pages(value did_gc, value n_pages)
 {
   CAMLparam2(did_gc, n_pages);
   size_t len = Int_val(n_pages) * PAGE_SIZE;
@@ -79,20 +79,5 @@ caml_mirage_iopage_alloc_pages(value did_gc, value n_pages)
   /* Explicitly zero the page before returning it */
   memset(block, 0, len);
 
-/* OCaml 4.02 introduced bigarray element type CAML_BA_CHAR,
-   which needs to be used - otherwise type t in io_page.ml
-   is different from the allocated bigarray and equality won't
-   hold.
-   Only since 4.02 there is a <caml/version.h>, thus we cannot
-   include it in order to detect the version of the OCaml runtime.
-   Instead, we use definitions which were introduced by 4.02 - and
-   cross fingers that they'll stay there in the future.
-   Once <4.02 support is removed, we should get rid of this hack.
-   -- hannes, 16th Feb 2015
- */
-#ifdef Caml_ba_kind_val
   CAMLreturn(caml_ba_alloc_dims(CAML_BA_CHAR | CAML_BA_C_LAYOUT | CAML_BA_MANAGED, 1, block, len));
-#else
-  CAMLreturn(caml_ba_alloc_dims(CAML_BA_UINT8 | CAML_BA_C_LAYOUT | CAML_BA_MANAGED, 1, block, len));
-#endif
 }
